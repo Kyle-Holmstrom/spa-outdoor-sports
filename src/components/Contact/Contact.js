2e4be159f50855f3c0d8a410e45762576af2015c
Kyle Holmstrom <holmstrom.kyle@yahoo.com>
2022-08-06 17:44:46 -0400

    changed the color schemes of the components

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,15 +4,17 @@ import './Contact.css';
 const Contact = () => {
     return (
         <div className="contact-container">
-            <h1>Start Your Adventure Today!</h1>
-            <h3>Contact an Adventure Expert!</h3>
-            <form>
-                <label>Name: <input type="text" name="name" /></label>
-                <label>Email: <input type="text" name="email" /></label>
-                <label>Message</label>
-                <textarea name="message" /><br/>
-                <input type="submit" value="Submit" className="submit-btn" />
-            </form>
+            <div className="contact-content">
+                <h1>Start Your Adventure Today!</h1>
+                <h3>Contact an Adventure Expert!</h3>
+                <form>
+                    <label>Name: <input type="text" name="name" /></label>
+                    <label>Email: <input type="text" name="email" /></label>
+                    <label>Message</label>
+                    <textarea name="message" /><br/>
+                    <input type="submit" value="Submit" className="submit-btn" />
+                </form>
+            </div>
         </div>
     );
 }
