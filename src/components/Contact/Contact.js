df6e1897aba36ad77fe1434cb2888ad29af363c2
Kyle Holmstrom <holmstrom.kyle@yahoo.com>
2022-08-25 08:45:52 -0400

    Added default text in the input fields of the contact form component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,10 +8,10 @@ const Contact = () => {
                 <h1>Start Your Adventure Today!</h1>
                 <h3>Contact an Adventure Expert!</h3>
                 <form>
-                    <label>Name: <input type="text" name="name" /></label>
-                    <label>Email: <input type="text" name="email" /></label>
+                    <label>Name: <input type="text" name="name" defaultValue="Full Name" /></label>
+                    <label>Email: <input type="text" name="email" defaultValue="john.doe@email.com" /></label>
                     <label>Message</label>
-                    <textarea name="message" /><br/>
+                    <textarea name="message" defaultValue="Type your message here." /><br/>
                     <input type="submit" value="Submit" className="submit-btn" />
                 </form>
             </div>
